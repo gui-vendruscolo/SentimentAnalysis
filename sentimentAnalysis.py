ce5ef1a7c753c95a7f2d9d6b460d4d420a7a4011
vedruscolo-gui <guilherme.vensilva@gmail.com>
2024-07-28 11:15:03 -0300

    versao teste

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -1,6 +1,7 @@
 import torch
 from torch.utils.data import DataLoader
-from transformers import BertTokenizer, BertForSequenceClassification, AdamW
+from torch.optim import AdamW
+from transformers import BertTokenizer, BertForSequenceClassification
 from datasets import load_dataset
 from tqdm import tqdm
 
@@ -20,12 +21,14 @@ test_data = test_data.map(tokenize_function, batched=True)
 train_data.set_format('torch', columns=['input_ids', 'attention_mask', 'label'])
 test_data.set_format('torch', columns=['input_ids', 'attention_mask', 'label'])
 
-train_loader = DataLoader(train_data, batch_size=64, shuffle=True)
-test_loader = DataLoader(test_data, batch_size=64, shuffle=False)
+train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=32, shuffle=False)
 
 batch = next(iter(train_loader))
 print(batch['input_ids'].shape)
 print(batch['label'].shape)
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2)
 model = model.to(device)
@@ -33,7 +36,7 @@ model = model.to(device)
 optimizer = AdamW(model.parameters(), lr=2e-5)
 criterion = torch.nn.CrossEntropyLoss()
 
-def trtain(model, iterator, optimizer, criterion, device):
+def train(model, iterator, optimizer, criterion, device):
     model.train()
     epoch_loss = 0
     epoch_acc = 0
@@ -42,7 +45,7 @@ def trtain(model, iterator, optimizer, criterion, device):
         optimizer.zero_grad()
         input_ids = batch['input_ids'].to(device)
         attention_mask = batch['attention_mask'].to(device)
-        labels = batch['labels'].to(device)
+        labels = batch['label'].to(device)
 
         outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
         loss = outputs.loss
@@ -53,3 +56,35 @@ def trtain(model, iterator, optimizer, criterion, device):
 
         epoch_loss += loss.item()
         epoch_acc += (logits.argmax(dim=1) == labels).sum().item() / len(labels)
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+def evaluate(model, iterator, criterion, device):
+    model.eval()
+    epoch_loss = 0
+    epoch_acc = 0
+
+    with torch.no_grad():
+        for batch in tqdm(iterator):
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = batch['label'].to(device)
+
+            outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+            loss = output.loss
+            logits = output.logits
+
+            epoch_loss += loss.item()
+            epoch_acc += (logits.argmax(dim=1) == labels).sum().item() / len(labels)
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+n_epochs = 3
+
+for epoch in range(n_epochs):
+    train_loss, train_acc = train(model, train_loader, optimizer, criterion, device)
+    valid_loss, valid_acc = evaluate(model, test_loader, criterion, device)
+
+    print(f'Epoch: {epoch+1}')
+    print(f'\tTrain Loss: {train_loss:.3f} | Train accuracy: {train_acc*100:.2f}%')
+    print(f'\tValid Loss: {valid_loss:.3f} | Valid accuracy: {valid_acc*100:.2f}%')
